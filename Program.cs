fa8b06350d76a9520b0a8361739b5b13ef18b191
AlexTEXHAPb <130362589+AlexTEXHAPb@users.noreply.github.com>
2023-06-05 22:16:06 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,30 +4,30 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-// int Read(string message) // Метод ввода
-// {
-//     Console.WriteLine(message);
-//     return Convert.ToInt32(Console.ReadLine());
-// }
+int Read(string message) // Метод ввода
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
-// void FillArray(double[,] array) // Метод заполнения и вывода на экран массива со случайными вещественными числами
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             array[i, j] = new Random().Next(-100,100)/10.0;
-//             System.Console.Write($"{array[i, j],6} ");
-//         }
-//     System.Console.WriteLine();
-//     }
-// }
+void FillArray(double[,] array) // Метод заполнения и вывода на экран массива со случайными вещественными числами
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100,100)/10.0;
+            System.Console.Write($"{array[i, j],6} ");
+        }
+    System.Console.WriteLine();
+    }
+}
 
-// Console.Clear();
-// int rows = Read("Введите количество строк массива");
-// int colums = Read("Введите количество столбцов массива");
-// double[,] RndArray = new double[rows, colums];
-// FillArray(RndArray);
+Console.Clear();
+int rows = Read("Введите количество строк массива");
+int colums = Read("Введите количество столбцов массива");
+double[,] RndArray = new double[rows, colums];
+FillArray(RndArray);
 
 // Задача 50. Напишите программу, которая на вход принимает значение элемента в двумерном массиве, 
 // и возвращает позицию этого элемента или же указание, что такого элемента нет.
@@ -138,67 +138,67 @@
 // 1 2 3 4
 // 5 7 9 10
 
-void SortArray(int[,] array)
-{
-    int[] array1 = new int[array.GetLength(0) * array.GetLength(1)];
-    int z = 0;
-    int[,] arraySort = new int[array.GetLength(0), array.GetLength(1)];
-    for (int i = 0; i < array.GetLength(0); i++)
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array1[z] = array[i, j];
-            z++;
-        }
-    Console.WriteLine("Отсортированный массив: ");
-    for (int i = 0; i < array1.Length; i++)
-    {
-        for (int j = 0; j < array1.Length - 1 - i; j++)
-        {
-            if (array1[j] > array1[j + 1])
-            {
-                (array1[j], array1[j + 1]) = (array1[j + 1], array1[j]);
-            }
-        }
-    }
-    z = 0;
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            arraySort[i, j] = array1[z];
-            z++;
-            Console.Write($" {arraySort[i, j],3}");
-        }
-        System.Console.WriteLine();
-    }
-}
+// void SortArray(int[,] array)
+// {
+//     int[] array1 = new int[array.GetLength(0) * array.GetLength(1)];
+//     int z = 0;
+//     int[,] arraySort = new int[array.GetLength(0), array.GetLength(1)];
+//     for (int i = 0; i < array.GetLength(0); i++)
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array1[z] = array[i, j];
+//             z++;
+//         }
+//     Console.WriteLine("Отсортированный массив: ");
+//     for (int i = 0; i < array1.Length; i++)
+//     {
+//         for (int j = 0; j < array1.Length - 1 - i; j++)
+//         {
+//             if (array1[j] > array1[j + 1])
+//             {
+//                 (array1[j], array1[j + 1]) = (array1[j + 1], array1[j]);
+//             }
+//         }
+//     }
+//     z = 0;
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             arraySort[i, j] = array1[z];
+//             z++;
+//             Console.Write($" {arraySort[i, j],3}");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
 
-void FillArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-        for (int j = 0; j < array.GetLength(1); j++)
-            array[i, j] = new Random().Next(-30, 30);
-}
+// void FillArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             array[i, j] = new Random().Next(-30, 30);
+// }
 
-void PrintArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            System.Console.Write($"{array[i, j],3} ");
-        }
-        System.Console.WriteLine();
-    }
-}
-Console.WriteLine("Введите количество строк");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите количество столбцов");
-int cols = Convert.ToInt32(Console.ReadLine());
-int[,] array = new int[rows, cols];
-FillArray(array);
-PrintArray(array);
-SortArray(array);
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             System.Console.Write($"{array[i, j],3} ");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+// Console.WriteLine("Введите количество строк");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int cols = Convert.ToInt32(Console.ReadLine());
+// int[,] array = new int[rows, cols];
+// FillArray(array);
+// PrintArray(array);
+// SortArray(array);
 
 // задача 2 HARD необязательная. Считается за четыре обязательных ) то есть три этих и одна с будущего семинара
 // Сгенерировать массив случайных целых чисел размерностью m*n (размерность вводим с клавиатуры), причем чтоб количество элементов было четное. 
